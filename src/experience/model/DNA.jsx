f193af2390601336e8bdcc9325c2b2923e93caf9
Pierre <pierre@concept-image.fr>
2024-08-27 14:41:16 +0200

    dna WIP

--- src/experience/model/DNA.jsx
+++ src/experience/model/DNA.jsx
@@ -1,24 +1,6 @@
 import { useGLTF } from "@react-three/drei";
 
 export default function DNA(props) {
-    const { nodes, materials } = useGLTF('model/dna.glb')
-	console.log(materials)
-
-	return (
-		<group {...props} dispose={null}>
-		<group rotation={[-0.013, 0.235, 2.339]}>
-			<group rotation={[Math.PI / 2, 0, 0]}>
-			<mesh
-				castShadow
-				receiveShadow
-				geometry={nodes.defaultMaterial.geometry}
-				material={materials['1001']}
-				position={[365.141, 569.14, 270.475]}
-				rotation={[-Math.PI / 2, 0, 0]}
-				scale={100}
-			/>
-			</group>
-		</group>
-		</group>
-	)
+    const DNA = useGLTF('model/human_dna.glb')
+	return <primitive object={DNA.scene} />
 }
