9933d41294db819d16a8632ecfd77475f7926f0b
Pierre <pierre@concept-image.fr>
2024-09-03 16:21:22 +0200

    scrolltrigger adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './sass/App.scss'
 import Frontpage from './frontpage/Frontpage'
+import './sass/composent/frame.scss'
 
 function App() {
 
 	return (
-		<>
+		<>	
+			<div className="frame"></div>
 			<Frontpage />
 		</>
 	)
--- src/frontpage/Frontpage.jsx
+++ src/frontpage/Frontpage.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import TemplatePage from "../components/TemplatePage";
 import FpAbout from "./FpAbout";
 import FpDNA from "./FpDNA";
@@ -5,13 +6,41 @@ import FpHero from "./FpHero";
 import FpLead from "./FpLead";
 import FpProducts from "./FpProducts";
 import Experience from "../experience/Experience";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import '../sass/FrontPage.scss';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Frontpage() {
+
+    const [ modelPosition, setModelPosition ] = useState(0);
+
+    useEffect(() => {
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: '.fpDNA',
+                start: 'top top',
+                end: 'bottom top',
+                markers: true,
+                onUpdate: (self) => {
+                    const progress = self.progress;
+                    setModelPosition(progress);
+                }
+            }
+        });
+          
+    }, []);
+
+    useEffect(() => {
+        console.log(modelPosition);
+    }, [modelPosition]);
+    
+
     return (
     <TemplatePage>
         <div className="experience">
-            <Experience />
+            <Experience modelPosition={modelPosition}/>
         </div>
 
         <div className="frontPage">
