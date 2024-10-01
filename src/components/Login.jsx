e52f35af66db487456ba3e158a888542627fb5e1
Pierre <pierre@concept-image.fr>
2024-10-01 13:42:09 +0200

    login page style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../sass/composent/Login.scss';
 
 function Login({ handleLogin }) {
     const [username, setUsername] = useState('');
@@ -16,28 +17,45 @@ function Login({ handleLogin }) {
         }
     };
 
+    const handleForgotPassword = (e) => {
+        e.preventDefault();
+        alert('Non ! ça va les alerter ! Il va falloir trouver le mot de passe du PDG');
+    }
+
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
+        <div className='loginPage'>
+            <div className='loginPage__container container'>
+
+                <div className='loginPage__wrapper'>
+
+                    <h2>Acces à l'Extranet</h2>
+
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <label>Identifiant:</label>
+                            <input
+                                type="text"
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <label>Mot de passe:</label>
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button type="submit">Connexion</button>
+                    </form>
+
+                    <a href="#" onClick={handleForgotPassword}>mot de passe oublié</a>
+
                 </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
+
+            </div>
+
         </div>
     );
 }
