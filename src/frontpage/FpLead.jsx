01129e1e83c65736bbc974caf001e32c6101f52e
Pierre <pierre.alberge@gmail.com>
2024-10-06 14:08:03 +0200

    lead created

--- src/frontpage/FpLead.jsx
+++ src/frontpage/FpLead.jsx
@@ -1,3 +1,4 @@
+import { TypeAnimation } from "react-type-animation";
 import "../sass/frontpage/fpLead.scss";
 
 export default function FpLead() {
@@ -6,7 +7,26 @@ export default function FpLead() {
             <div className="fpLead">
                 <div className="container">
                     <div className="fpLead__content">
-            
+                    <TypeAnimation
+                        sequence={[
+                            // Same substring at the start will only be typed once, initially
+                            'Our sponsorship : Black Rock',
+                            800,
+                            'Our sponsorship : Starlink',
+                            800,
+                            'Our sponsorship : Enron',
+                            800,
+                            'Our sponsorship : Amalgamated Bio-Carb, inc.',
+                            800,
+                            'Our sponsorship : Hunt Technologist, inc.',
+                            800,
+                            'Our sponsorship : March Technologies',
+                            800
+                        ]}
+                        speed={50}
+                        style={{ fontSize: '2em' }}
+                        repeat={Infinity}
+                    />
                     </div>
                 </div>
             </div>
--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -5,7 +5,11 @@ export default function Footer() {
     <footer className="footer">
         <div className="container">
             <div className="footer__content">
-                footer
+                <div className="footer__content__left">
+                    <img className="footer__content__left__logo" src="logo.png" alt="logo" />
+                    <h3>Get in touch</h3>
+                    <p>Feel free to contact us for any questions or inquiries</p>
+                </div>
             </div>
         </div>
     </footer>
