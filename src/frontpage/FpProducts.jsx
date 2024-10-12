15b7d94676cc454968e487b15d92c75e56153b80
Pierre <pierre.alberge@gmail.com>
2024-10-12 20:37:26 +0200

    trame

--- src/frontpage/FpProducts.jsx
+++ src/frontpage/FpProducts.jsx
@@ -13,6 +13,11 @@ export default function FpProducts() {
     const { load } = useAudioPlayer();
     const [ startCom, setStartCom ] = useState(0);
     const [ window1, setWindow1 ] = useState(false);
+    const [ window2, setWindow2 ] = useState(false);
+    const [ window3, setWindow3 ] = useState(false);
+    const [ window4, setWindow4 ] = useState(false);
+    const [ window5, setWindow5 ] = useState(false);
+    const [ window6, setWindow6 ] = useState(false);
 
     useEffect(() => {   
         ScrollTrigger.create({
@@ -56,6 +61,26 @@ export default function FpProducts() {
                     <div className="fpProducts__content__chat">
                     {window1 &&
                         <WindowImage className="window1" name="error" source="error.jpg" />
+                    }
+
+                    {window2 &&
+                        <WindowImage className="window2" name="brain" source="brain.gif" />
+                    }
+
+                    {window3 &&
+                        <WindowImage className="window3" name="logo" source="logo.png" />
+                    }
+
+                    {window4 &&
+                        <WindowImage className="window4" name="logo" source="breaking-spy.gif" />
+                    }
+
+                    {window5 &&
+                        <WindowImage className="window5" name="plane" source="plane.gif" />
+                    }
+
+                    {window6 &&
+                        <WindowImage className="window6" name="logo" source="eg.png" />
                     }
 
                     {com &&
@@ -116,8 +141,66 @@ export default function FpProducts() {
                                 () => {
                                     const audio4 = document.getElementById('audio4');
                                     audio4.play();
+                                    setWindow2(true);
                                 },
-                            
+                                6000,
+                                'Vous êtes unique',
+                                () => {
+                                    const audio5 = document.getElementById('audio5');
+                                    audio5.play();
+                                },
+                                5000,
+                                'Johnson Meditech a fait une découverte',
+                                () => {
+                                    const audio6 = document.getElementById('audio6');
+                                    audio6.play();
+                                    setWindow3(true);
+                                },
+                                12000,
+                                'J\'ai été compromis',
+                                () => {
+                                    const audio7 = document.getElementById('audio7');
+                                    audio7.play();
+                                    setWindow4(true);
+                                },
+                                9000,
+                                'Qu\'est-ce que HumanDotNet ?',
+                                () => {
+                                    const audio8 = document.getElementById('audio8');
+                                    audio8.play();
+                                },
+                                10000,
+                                'Je suis un spyware !',
+                                () => {
+                                    const audio9 = document.getElementById('audio9');
+                                    audio9.play();
+                                },
+                                18000,
+                                'Il est temps d\'enquêter sur leurs projets',
+                                () => {
+                                    const audio10 = document.getElementById('audio10');
+                                    audio10.play();
+                                    setWindow5(true);
+                                },
+                                17000,
+                                'Il faut me sortir de là et vite !',
+                                () => {
+                                    const audio11 = document.getElementById('audio11');
+                                    audio11.play();
+                                },
+                                21000,
+                                'Il nous faut les identifiants de connexion d\'Edmund',
+                                () => {
+                                    const audio12 = document.getElementById('audio12');
+                                    audio12.play();
+                                },
+                                19000,
+                                'https://eg-real-estate.vercel.app',
+                                () => {
+                                    const audio13 = document.getElementById('audio13');
+                                    audio13.play();
+                                    setWindow6(true);
+                                },
                             ]}
                             deletionSpeed={80}
                             speed={50}
